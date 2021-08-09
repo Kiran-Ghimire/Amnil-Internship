e5a9b1f967687754305c5c878948745b9f99227b
N4secnT <54986538+N4secnT@users.noreply.github.com>
2021-08-09 09:45:46 +0545

    COM#2: Making tax dynamic by inserting taxSlabs array

--- net-salary-calculator.js
+++ net-salary-calculator.js
@@ -5,7 +5,52 @@ const persons = [
   { name: "Josh Doe", grossSalary: 37000 },
 ];
 
+//Test case: besides provided tax slab, one should be able to update taxInPercentage or add/remove taxSlab 
+
+const taxSlabs = [
+  { gt: 0, lte: 10000, tax: 2},
+  { gt: 10000, lte: 20000, tax: 4},
+  { gt: 20000, lte: 30000, tax: 7},
+  { gt: 30000, lte: 9999999999, tax: 10},
+];
+
+
+const taxCalculator = (salary) => {
+  let tax = 0;
+  for (let i = 0; i < taxSlabs.length; i++){
+    if ( salary > 0 && salary < 10000){
+      tax = taxSlabs.filter((data, i) => (data.gt == 0 && data.lte <= 10000))[0].tax
+      
+      break;
+    }else if ( salary >= 10000 && salary < 20000){
+      
+      tax = taxSlabs.filter((data, i) => (data.gt == 10000 && data.lte <= 20000))[0].tax
+      
+      break;
+    }else if (salary >= 20000 && salary < 30000){
+      tax = taxSlabs.filter((data, i) => (data.gt == 20000 && data.lte <= 30000))[0].tax
+      
+      break;
+    }else if (salary >= 30000 && salary <= 9999999999 ) {
+
+      
+      tax = taxSlabs.filter((data, i) => (data.gt == 30000 && data.lte <= 9999999999))[0].tax
+      
+      break;
+    } 
+
+  }
+
+return tax;
+}
+// console.log(taxCalculator(5000));
+
+
+
 const salaryCalculator = () => {
+
+  
+
   let personsNetSalary = [];
   persons.forEach((person, index) => {
     const { name, grossSalary } = person;
@@ -13,35 +58,32 @@ const salaryCalculator = () => {
     let netSalary = 0;
 
     if (grossSalary > 0 && grossSalary <= 10000) {
-      tax = 0.02 * grossSalary;
+      tax = (taxCalculator(grossSalary)/100) * grossSalary;
       netSalary = grossSalary - tax;
     } else if (grossSalary > 10000 && grossSalary <= 20000) {
-      taxAmount1 = 10000 * 0.02;
-      taxAmount2 = (grossSalary - 10000) * 0.04;
-      tax = taxAmount1 + taxAmount2;
+      tax = (taxCalculator(grossSalary)/100) * grossSalary;
       netSalary = grossSalary - tax;
     } else if (grossSalary > 20000 && grossSalary <= 30000) {
-      taxAmount1 = 10000 * 0.02;
-      taxAmount2 = 20000 * 0.04;
-      taxAmount3 = (grossSalary % 20000) * 0.07;
-      tax = taxAmount1 + taxAmount2 + taxAmount3;
+      tax = (taxCalculator(grossSalary)/100) * grossSalary;
       netSalary = grossSalary - tax;
-    } else if (grossSalary > 30000) {
-      taxAmount1 = 10000 * 0.02;
-      taxAmount2 = 20000 * 0.04;
-      taxAmount3 = 30000 * 0.07;
-      taxAmount4 = (grossSalary - 30000) * 0.1;
-      tax = taxAmount1 + taxAmount2 + taxAmount3 + taxAmount4;
+    } else if (grossSalary > 30000 && grossSalary <= 9999999999 ) {
+      tax = (taxCalculator(grossSalary)/100) * grossSalary;
       netSalary = grossSalary - tax;
     } else {
-      console.log("Please input proper value");
+      console.log("Please input proper value", grossSalary);
+
     }
 
     console.log(`Net salary of ${persons[index].name} is ${netSalary}`);
 
+
     personsNetSalary.push({ name: name, netSalary: netSalary });
   });
+  taxCalculator();
   return personsNetSalary;
 };
 
 console.log(salaryCalculator());
+
+
+
