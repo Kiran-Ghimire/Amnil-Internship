8faaa9bc196938be498191b4dca3af7356e83828
N4secnT <54986538+N4secnT@users.noreply.github.com>
2021-08-11 14:30:34 +0545

    COM#3 -  Returns cube of a number

--- javascript-exercises-1.js
+++ javascript-exercises-1.js
@@ -1,4 +1,4 @@
-//Task 1
+//Ex1: Start
 
 String.prototype.isPalindrome = function () {
   var cleaned = this.toLowerCase().match(/[a-z]/gi).reverse();
@@ -10,3 +10,18 @@ let word1 = "level";
 let word2 = "shovel";
 console.log(word1.isPalindrome());
 console.log(word2.isPalindrome());
+
+//Ex1: End
+
+//Ex 2: Start
+
+var myNum = new Number();
+
+Number.prototype.cube = function (x) {
+  let numSqr;
+  numSqr = x ** 3;
+  return numSqr;
+};
+console.log(` ${myNum.cube(2)} `);
+
+//Ex 2: End
