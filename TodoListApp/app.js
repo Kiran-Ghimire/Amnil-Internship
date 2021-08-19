ff79c84b0467d2660ebd0398bef858f481e05bec
N4secnT <54986538+N4secnT@users.noreply.github.com>
2021-08-19 23:57:30 +0545

    COM#4 fixing all the errors and adding import

--- TodoListApp/app.js
+++ TodoListApp/app.js
@@ -1,6 +1,8 @@
 import localServices from "./localStorage.js";
+let searchTodo = [];
 
 //Selectors
+
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
@@ -90,7 +92,7 @@ function deleteCheck(e) {
 
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
-    removeLocalTodos(todo);
+    localServices.removeLocalTodos(todo);
     todo.remove();
   }
 
@@ -222,3 +224,40 @@ function sortTodos(event) {
     todoList.appendChild(todoDiv);
   });
 }
+
+function renderTodos(todos) {
+  todos.forEach(function (todo) {
+    //Create todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    todoDiv.classList.add(todo[1]);
+
+    //Create list
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todo[0] + " " + todo[2] + " " + "upto" + " " + todo[3];
+    newTodo.dataset.startTime = todo[2];
+    newTodo.dataset.endTime = todo[3];
+
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    searchTodo.push([todo[0], todo[2], todo[3]]);
+
+    // todoInput.value = "";
+    //Create Completed Button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //Create trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //attach final Todo
+    todoList.appendChild(todoDiv);
+  });
+}
+
+export default renderTodos;
--- TodoListApp/localStorage.js
+++ TodoListApp/localStorage.js
@@ -1,3 +1,5 @@
+import renderTodos from "./app.js";
+
 function saveLocalTodos(todo, completedtodo, startingTime, endingTime) {
   let todos;
   if (localStorage.getItem("todos") === null) {
@@ -24,52 +26,20 @@ function removeLocalTodos(todo) {
 }
 
 function clearLocalTodos() {
-  todos = [];
+  let todos = [];
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function getTodos() {
   let todos;
-  searchTodo = [];
-  const todoList = document.querySelector(".todo-list");
+
   if (localStorage.getItem("todos") === null) {
     todos = [];
   } else {
     todos = JSON.parse(localStorage.getItem("todos"));
   }
 
-  todos.forEach(function (todo) {
-    //Create todo div
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
-
-    todoDiv.classList.add(todo[1]);
-
-    //Create list
-    const newTodo = document.createElement("li");
-    newTodo.innerText = todo[0] + " " + todo[2] + " " + "upto" + " " + todo[3];
-    newTodo.dataset.startTime = todo[2];
-    newTodo.dataset.endTime = todo[3];
-
-    newTodo.classList.add("todo-item");
-    todoDiv.appendChild(newTodo);
-
-    searchTodo.push([todo[0], todo[2], todo[3]]);
-
-    // todoInput.value = "";
-    //Create Completed Button
-    const completedButton = document.createElement("button");
-    completedButton.innerHTML = `<i class="fas fa-check"></i>`;
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
-    //Create trash button
-    const trashButton = document.createElement("button");
-    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
-    trashButton.classList.add("trash-btn");
-    todoDiv.appendChild(trashButton);
-    //attach final Todo
-    todoList.appendChild(todoDiv);
-  });
+  renderTodos(todos);
 }
 
 export default {
